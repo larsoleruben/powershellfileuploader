ed049b4c35374722de13e4fd6d8bb07d46f85a50
Lars Ole Christoffersen <lars.christoffersen@rubenc.dk>
2020-05-20 18:55:01 +0200

    Added recursion to the stuff

--- file_uploader.ps1
+++ file_uploader.ps1
@@ -5,13 +5,40 @@ function Get-TimeStamp {
     
 }
 
+
+function CreateDirectoriesFromPath ($DirectoryPath, $ServerPath, $Login, $Password){
+ 
+	$DirectoryParts = $DirectoryPath.Split("/");
+ 
+	$Position = "";
+	
+	for ($i = 0; $i -lt $DirectoryParts.Length - 1; $i++){
+		
+		try {
+			$Position += $DirectoryParts[$i] + "/";	
+			$AbsoluteTemporaryPath = New-Object System.Uri($ServerPath+$Position);
+			$WebRequest = [System.Net.WebRequest]::Create($AbsoluteTemporaryPath);
+			$WebRequest.KeepAlive = $false;
+			$WebRequest.Credentials = New-Object System.Net.NetworkCredential($Login, $Password);
+			$WebRequest.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory;
+			$WebRequest.GetResponse();
+			
+		} catch [Net.WebException] {
+            #If the directory already exits, it throws an exception and the program continues
+            #Could be nicer to test for existence, but!! 
+			Write-Host $_.Exception.Message; continue;
+		}
+	}
+}
+
 $logfilename = "ftplog.txt"
-$ftpURL = ""
-$ftpUsername = ""
-$ftpPwd = ""
+$ftpURL = "ftp://ftp.recykler.dk/powershelltest"
+$ftpUsername = "recykler.dk"
+$ftpPwd = "ps1Test2020"
+
 
 Write-Output "$(Get-TimeStamp) FTP Filetransfer started" | Out-file $logfilename -append
-$files = Get-ChildItem "/tmp" -Filter *.log
+$files = Get-ChildItem "/tmp" -File -Recurse -Filter *.log
 [string[]]$arrayFromFile = Get-Content -Path $logfilename
 foreach ($f in $files){
     #Write-Output "$(Get-TimeStamp) FTP Filetransfer started" | Out-file $logfilename -append
@@ -19,10 +46,16 @@ foreach ($f in $files){
     $filename = $f.Name
     $filename 
     $outfile
+    $ftpURL+$outfile
     #read the log to see if the file as allready been transferred
     if( (@($arrayFromFile) -like "*$outfile*").Count -eq 0 ){
+        #create the directory
+        $FtpPath = New-Object System.Uri($ftpURL+$outfile);
+        $ServerPath = "ftp://" + $FtpPath.Host;
+        $DirectoryPath = $FtpPath.LocalPath;
+        CreateDirectoriesFromPath $DirectoryPath $ServerPath $ftpUsername $ftpPwd;
         # create the FtpWebRequest and configure it
-        $ftp = [System.Net.FtpWebRequest]::Create("$ftpURL/$filename")
+        $ftp = [System.Net.FtpWebRequest]::Create($ftpURL+$outfile)
         $ftp = [System.Net.FtpWebRequest]$ftp
         $ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
         $ftp.Credentials = new-object System.Net.NetworkCredential($ftpUsername,$ftpPwd)
